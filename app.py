22f6e8c1887fa4379de34d0daa1d865bce0c0515
osCj35BqWr <35219434+osCj35BqWr@users.noreply.github.com>
2020-11-22 22:32:01 +0900

    とりあえずグラフ表示

--- app.py
+++ app.py
@@ -1,19 +1,48 @@
-# ライブラリ呼び出し
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
+from io import BytesIO
+import urllib
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 
-# クラス呼び出し
+import random
+import numpy as np
+
 app = Flask(__name__)
 
-# ルーティングを定義
+fig = plt.figure()
+ax = fig.add_subplot()
+
+x = np.arange(0, 100, 0.1)
+y = x ** 2
+
+
 @app.route('/')
-def hello_World():
-    return 'HelloWorld！'
+def index():
+    plt.cla()
+
+    plt.title('Graph')
+    plt.legend()
+    plt.grid()
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.plot(x, y)
+
+    canvas = FigureCanvasAgg(fig)
+    png_output = BytesIO()
+    canvas.print_png(png_output)
+    data = png_output.getvalue()
+
+    response = make_response(data)
+    response.headers['Content-Type'] = 'image/png'
+
+    return response
 
 # /hello(http://127.0.0.1:5000/hello)にアクセスするとsample.htmlの中身が表示される
 @app.route('/hello')
 def hello():
     return render_template("sample.html")
 
-
-# デバックモードでアプリを起動
-app.run(debug=True)
+if __name__ == '__main__':
+    # デバックモードでアプリを起動
+    app.run(debug=True)
