363d9b9b8914213ac926b583c1c97577a4792052
osCj35BqWr <35219434+osCj35BqWr@users.noreply.github.com>
2020-11-23 18:17:22 +0900

    一旦url_forの書き方で動いた（javascript)

--- flask_blog/static/script.js
+++ flask_blog/static/script.js
@@ -2,7 +2,12 @@ function getImg(obj) {
     var idx = obj.selectedIndex;
     var value = obj.options[idx].value;
     var plotdata = document.getElementById('s3img');
-    $.get("/img/" + value, function(data) {
+
+    var eventDataBaseUrl = "{{ url_for('img', 'DUMMY_VALUE') }}"
+    var actualDataUrl = eventDataBaseUrl.replace('DUMMY_VALUE', value)
+    alert(actualDataUrl)
+
+    $.get(actualDataUrl, function(data) {
         plotdata.src = "data:image/jpeg;base64," + data;
     });
 };
